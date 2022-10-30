Bug in scaling

Canvas.py
@@ -64,8 +64,9 @@ class Canvas:
 
         current_frame = self.capture()
         input_source = self._get_input(current_frame)
+        input_source = cv2.resize(input_source, (projector.screen_res[0], projector.screen_res[1]))
         mask = self.pre_processor.process(input_source.copy(), self.gui, self.monitor)
-
+        print(mask.shape)
         self.monitor.display()
         video_source = self.source.frame()
 
@@ -75,15 +76,17 @@ class Canvas:
             video_source = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
         video_source_mask_size = cv2.resize(video_source, (mask.shape[1], mask.shape[0]))
         mask_applied = np.where(mask_color[:, :] == [0, 0, 0], mask_color, video_source_mask_size)
-
+        print('applied', mask_applied.shape)
         full_canvas = np.zeros([projector.screen_res[1], projector.screen_res[0], 3], dtype=np.uint8)
+        print('full canvas', mask_applied.shape)
 
         offset_x = (0 - math.ceil(self.gui.max_offset_x / 2)) + self.gui.offset_x
         offset_y = (0 - math.ceil(self.gui.max_offset_y / 2)) + self.gui.offset_y
         # print('offsetx', offset_x)
         # print('offsety', offset_y)
 
-        full_canvas[self.top_y+offset_y:self.bottom_y+offset_y + 1, self.top_x+offset_x:self.bottom_x+offset_x + 1] = mask_applied
+        full_canvas = mask_applied
+        # full_canvas[self.top_y+offset_y:self.bottom_y+offset_y + 1, self.top_x+offset_x:self.bottom_x+offset_x + 1] = mask_applied
 
         if self.gui.replace_black:
             # Find all black pixels

Init.py
@@ -28,14 +28,19 @@ class Init:
             print("picture")
             self._white_frame = cam.picture()
         elif running_time < 3:
+            print("wait black")
             projector.black()
         else:
+            print("calibrate 1")
             # cv2.imshow("White", self._white_frame)
             white_fullcolor = np.copy(self._white_frame)
+            print("source shape: ", white_fullcolor.shape)
             white_bgr = cv2.cvtColor(self._white_frame, cv2.COLOR_BGR2GRAY)
-
+            print("calibrate 2")
 #            white_bgr = cv2.GaussianBlur(white_bgr, (5, 5), 0)
+            print("start thresh")
             ret, thresh = cv2.threshold(white_bgr, gui.calibration_threshold, 255, cv2.THRESH_BINARY)
+            print("start comt")
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             contour_count = len(contours)
             print("Found ", contour_count, "  during init state")
@@ -47,18 +52,22 @@ class Init:
             # Create mask where white is what we want, black otherwise
             mask = np.zeros_like(white_bgr)
             # -----------------
+            print("sort")
             sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
             largest_contour = sorted_contours[0]
 
             # ------------
+            print("draw largest")
             cv2.drawContours(mask, largest_contour, -1, 255, -1)
 
             if gui.calibration_show_found_contours:
-                cv2.drawContours(white_fullcolor, largest_contour, -1, (255, 255, 255), 3)
-                for contour in contours:
-                    cv2.drawContours(white_fullcolor, contour, -1, (randrange(255), randrange(255), randrange(255)), 3)
+                print("show found")
+                cv2.drawContours(white_fullcolor, largest_contour, -1, (255, 0, 0), 3)
+                # for contour in contours:
+                #    cv2.drawContours(white_fullcolor, contour, -1, (randrange(255), randrange(255), randrange(255)), 3)
 
             if gui.calibration_convex_hull:
+                print("show convex 10")
                 for contour in contours[0:10]:
                     hull = cv2.convexHull(contour)
                     cv2.fillConvexPoly(white_fullcolor, hull, 255)
@@ -87,5 +96,6 @@ class Init:
 
             self.canvas = Canvas()
             # print("INit canvas")
+            print("init canvas")
             self.canvas.init(top_y, bottom_y, top_x, bottom_x, out, gui)
             self.initialized = True