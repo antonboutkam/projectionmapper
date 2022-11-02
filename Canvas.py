cec279f5b98cd591d5101a06be70e637e0dc31cd
Anton Boutkam <anton@nui-boutkam.nl>
2022-11-03 00:10:13 +0100

    Try to add support for multiple objects with separate videos

--- Canvas.py
+++ Canvas.py
@@ -213,10 +213,12 @@ class Canvas:
             base_mask_bgr = gpu_mask_bgr.download()
             print("Seeking contours ")
             contours, hierarchy = cv2.findContours(base_mask_bgr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
             print("Found: " + str(len(contours)) + ' contours')
             blank_mask = np.zeros_like(base_mask_bgr)
             print("Desired contours: ", self.gui.draw_contour_min , " to ", self.gui.draw_contour_min)
-            for index, contour in enumerate(contours[self.gui.draw_contour_min:self.gui.draw_contour_max]):
+            for index, contour in enumerate(sorted_contours[self.gui.draw_contour_min:self.gui.draw_contour_max]):
                 print("Drawing: " + str(index) + ' contour')
                 drawn_mask = cv2.drawContours(blank_mask, contour, -1, 255, -1)
                 self.monitor.add("Drawn contour " + str(index), drawn_mask)
