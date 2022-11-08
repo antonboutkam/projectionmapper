0890b77c1999d413bad12f2884fe85abc5ca8248
Anton Boutkam <anton@nui-boutkam.nl>
2022-11-08 23:26:41 +0100

    x

--- Art.py
+++ Art.py
@@ -4,11 +4,11 @@ import cv2
 class Art:
     def draw_coolness(self, canvas, contours):
         for contour in contours:
-            canvas = self.draw_skip_lines(canvas, contour, 2, (0, 255, 0), 3, 0)
-            canvas = self.draw_skip_lines(canvas, contour, 2, (255, 0, 0), 3, 1)
+            canvas = self.draw_skip_lines(canvas, contour, 2, (0, 255, 0), 1)
+            canvas = self.draw_skip_lines(canvas, contour, 2, (255, 0, 0), 1)
         return canvas
 
-    def draw_skip_lines(self, canvas, contour, step, color, thickness, start):
+    def draw_skip_lines(self, canvas, contour, step, color, thickness):
         n = contour.ravel()
         i = 0
         lines = []
@@ -27,20 +27,8 @@ class Art:
                     continue
                 else:
                     cv2.line(canvas, (prev_x, prev_y), (x, y), color, thickness)
-            i = i + 1
-        i = 0
-        for j in n:
-            if i < start:
-                continue
-            if (i % step) == 0:
-                x = n[i]
-                y = n[i + 1]
-
-                if prev_x is None:
                     prev_x = x
                     prev_y = y
-                    continue
-                else:
-                    cv2.line(canvas, (prev_x, prev_y), (x, y), color, thickness)
             i = i + 1
+
         return canvas
