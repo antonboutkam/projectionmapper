302d1e81b102025d76a04199529263f8f3557fd0
Anton Boutkam <anton@nui-boutkam.nl>
2022-11-09 00:01:32 +0100

    x

--- Art.py
+++ Art.py
@@ -4,29 +4,35 @@ import cv2
 class Art:
     def draw_coolness(self, canvas, contours):
         for contour in contours:
-            canvas = self.draw_skip_lines(canvas, contour, 2, (0, 255, 0), 1)
-            # canvas = self.draw_skip_lines(canvas, contour, 3, (255, 0, 0), 1)
+            canvas = self.draw_skip_lines(canvas, contour, 2, (0, 255, 0), 1, 0)
+            canvas = self.draw_skip_lines(canvas, contour, 3, (255, 0, 0), 1, 2)
         return canvas
 
-    def draw_skip_lines(self, canvas, contour, step, color, thickness):
+    def draw_skip_lines(self, canvas, contour, step, color, thickness, start):
         n = contour.ravel()
         i = 0
         lines = []
         prev_x = None
         prev_y = None
-        for j in n:
-            if (i % step) == 0:
-                x = n[i]
-                y = n[i + 1]
-
-                if prev_x is None:
-                    prev_x = x
-                    prev_y = y
+        while True:
+            if i == len(contour):
+                break
+            for j in n:
+                if i < start:
+                    i = i + 1
                     continue
-                else:
-                    cv2.line(canvas, (prev_x, prev_y), (x, y), color, thickness)
-                    prev_x = x
-                    prev_y = y
-            i = i + 1
+                if (i % step) == 0:
+                    x = n[i]
+                    y = n[i + 1]
+
+                    if prev_x is None:
+                        prev_x = x
+                        prev_y = y
+                        continue
+                    else:
+                        cv2.line(canvas, (prev_x, prev_y), (x, y), color, thickness)
+                        prev_x = x
+                        prev_y = y
+                i = i + 1
 
         return canvas
