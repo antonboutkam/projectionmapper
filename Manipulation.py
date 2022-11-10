3693f91e1edde4894e71c30a5f715aeb074c0ba4
Anton Boutkam <anton@nui-boutkam.nl>
2022-11-10 22:00:33 +0100

    Added lots of new features

---
+++ Manipulation.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Manipulation:
+
+    @staticmethod
+    def color_reduction(frame_rgb, red, green, blue):
+        if red > 0 or green > 0 or blue > 0:
+            frame_black = np.zeros_like(frame_rgb)
+            frame_black[..., [0]] = red
+            frame_black[..., [1]] = green
+            frame_black[..., [2]] = blue
+            frame_rgb = frame_rgb.substract(frame_black)
+        return frame_rgb
+
+    @staticmethod
+    def area_of_interest(frame_rgb, cut_l, cut_r, cut_t, cut_b):
+        if cut_l > 0 or cut_r > 0 or cut_t > 0 or cut_b > 0:
+            frame_black = np.zeros_like(frame_rgb)
+            s = frame_black.shape
+
+            y_t = cut_t
+            y_b = s[0] - cut_b
+            x_l = cut_l
+            x_r = s[1] - cut_r
+
+            frame_black[y_t:y_b, x_l:x_r] = frame_rgb[y_t:y_b, x_l:x_r]
+            return frame_black
+        return frame_rgb
